f4662f6109c02a90b7dc474da42eddcd4c806d90
Yuichi-TanakaJP <yuta.zip@gmail.com>
2026-09-15 23:14:41 +0900

    refactor(yutai-expiry): make daily dashboard the default DB preview UI

--- app/tools/yutai-expiry/ClientOnly.tsx
+++ app/tools/yutai-expiry/ClientOnly.tsx
@@ -7,18 +7,11 @@ type Props = { scanEnabled: boolean };
 // scanEnabled は server component が cookie 検証して props で渡す前提なので、
 // 共通の createClientOnlyTool は使わずインラインで dynamic 化する。
 const ToolClient = dynamic(() => import("./ToolClient"), { ssr: false });
-const DatabaseRewardsGate = dynamic(() => import("./DatabaseRewardsGate"), { ssr: false });
-const RewardLedgerV2Workspace = dynamic(
-  () => import("./RewardLedgerV2Workspace").then((module) => module.RewardLedgerV2Workspace),
-  { ssr: false }
-);
+const YutaiExpiryDashboard = dynamic(() => import("./YutaiExpiryDashboard"), { ssr: false });
 
 export default function ClientOnly(props: Props) {
   if (process.env.NEXT_PUBLIC_YUTAI_EXPIRY_DB_PREVIEW === "true") {
-    return <>
-      <DatabaseRewardsGate {...props} />
-      <RewardLedgerV2Workspace />
-    </>;
+    return <YutaiExpiryDashboard {...props} />;
   }
   return <ToolClient {...props} />;
 }
